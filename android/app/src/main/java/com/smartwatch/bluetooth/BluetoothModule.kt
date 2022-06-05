4c8c3d6faca2e221f2a8c5646a1cfae860645fc5
Paula Rolim <rolim.paula1999@gmail.com>
2022-06-05 17:37:20 -0300

    feat: run native methods from javascript

--- android/app/src/main/java/com/smartwatch/bluetooth/BluetoothModule.kt
+++ android/app/src/main/java/com/smartwatch/bluetooth/BluetoothModule.kt
@@ -5,9 +5,15 @@ import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.bridge.ReactContextBaseJavaModule
 import com.facebook.react.bridge.ReactMethod
 import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothDevice
 import android.bluetooth.BluetoothManager
+import android.bluetooth.le.BluetoothLeScanner
 import android.os.Build
+import android.os.Handler
+import android.os.Looper
 import androidx.annotation.RequiresApi
+
+const val DISCOVERY_TIME_DELAY_IN_MS = 15000
 
 class BluetoothModule(reactApplicationContext: ReactApplicationContext) :
     ReactContextBaseJavaModule(reactApplicationContext) {
@@ -17,13 +23,68 @@ class BluetoothModule(reactApplicationContext: ReactApplicationContext) :
 
     @RequiresApi(Build.VERSION_CODES.M)
     @ReactMethod
-    fun hasBluetoothSupport() {
+    fun hasBluetoothSupport(): Boolean  {
         val bluetoothManager: BluetoothManager = reactApplicationContext.getSystemService(BluetoothManager::class.java)
-        val bluetoothAdapter: BluetoothAdapter? = bluetoothManager.adapter
-        if (bluetoothAdapter == null) {
+        val bluetoothAdapter: BluetoothAdapter = bluetoothManager.adapter
+        return if (bluetoothAdapter == null) {
             Log.d("BluetoothConnection", "Sem suporte ao bluetooth")
+            false
         } else {
             Log.d("BluetoothConnection", "Com suporte ao bluetooth")
+            true
         }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.M)
+    @ReactMethod
+    fun isBluetoothEnabled(): Boolean {
+        val bluetoothManager: BluetoothManager = reactApplicationContext.getSystemService(BluetoothManager::class.java)
+        val bluetoothAdapter: BluetoothAdapter = bluetoothManager?.adapter
+
+        return if (bluetoothAdapter?.isEnabled) {
+            Log.d("BluetoothConnection", "Bluetooth habilitado")
+            true
+        } else {
+            Log.d("BluetoothConnection", "Bluetooth desabilitado")
+            false
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.M)
+    @ReactMethod
+    fun listPairedDevices() {
+        val bluetoothManager: BluetoothManager = reactApplicationContext.getSystemService(BluetoothManager::class.java)
+        val bluetoothAdapter: BluetoothAdapter = bluetoothManager?.adapter
+
+        val hasBluetoothSupport = hasBluetoothSupport()
+        if (hasBluetoothSupport) {
+            val pairedDevices: Set<BluetoothDevice>? = bluetoothAdapter?.bondedDevices
+            pairedDevices?.forEach { device ->
+                val deviceName = device.name
+                val deviceHardwareAddress = device.address // MAC address
+                Log.d("BluetoothConnection", "Device $deviceName - $deviceHardwareAddress")
+            }
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.M)
+    @ReactMethod
+    fun listDiscoveryDevices() {
+        val bluetoothManager: BluetoothManager = reactApplicationContext.getSystemService(BluetoothManager::class.java)
+        val bluetoothAdapter: BluetoothAdapter = bluetoothManager?.adapter
+
+        val deviceScanCallback: DeviceScanCallback = DeviceScanCallback()
+        val bluetoothScanner: BluetoothLeScanner = bluetoothAdapter.bluetoothLeScanner
+
+        if (bluetoothScanner != null) {
+            Log.d("BluetoothConnection", "Buscando dispositivos...")
+            bluetoothScanner.startScan(deviceScanCallback)
+        }
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            bluetoothAdapter?.bluetoothLeScanner.stopScan(deviceScanCallback)
+            Log.d("BluetoothConnection", "Busca encerrada")
+            deviceScanCallback.getDiscoveredDevices()
+        }, DISCOVERY_TIME_DELAY_IN_MS.toLong())
     }
 }
--- src/screens/ListDevices/ListDevices.tsx
+++ src/screens/ListDevices/ListDevices.tsx
@@ -1,9 +1,56 @@
 import React from 'react';
-import {NativeModules, Text} from 'react-native';
+import {
+  NativeModules,
+  Text,
+  TouchableOpacity,
+  TouchableOpacityProps,
+  View,
+} from 'react-native';
+
+const Button = ({children, onPress}: TouchableOpacityProps) => {
+  return (
+    <TouchableOpacity
+      onPress={onPress}
+      style={{backgroundColor: 'red', padding: 10, marginVertical: 5}}>
+      <Text>{children}</Text>
+    </TouchableOpacity>
+  );
+};
 
 export const ListDevices = () => {
-  const checkBridge = NativeModules.BluetoothConnection.hasBluetoothSupport;
-  checkBridge();
+  return (
+    <View style={{padding: 24}}>
+      <Text>List</Text>
 
-  return <Text>List</Text>;
+      <Button onPress={() => {}}>Request</Button>
+
+      <Button
+        onPress={() => {
+          NativeModules.BluetoothConnection.isBluetoothEnabled();
+        }}>
+        isBluetoothEnabled
+      </Button>
+
+      <Button
+        onPress={() => {
+          NativeModules.BluetoothConnection.listPairedDevices();
+        }}>
+        listPairedDevices
+      </Button>
+
+      <Button
+        onPress={() => {
+          NativeModules.BluetoothConnection.hasBluetoothSupport();
+        }}>
+        hasBluetoothSupport
+      </Button>
+
+      <Button
+        onPress={() => {
+          NativeModules.BluetoothConnection.listDiscoveryDevices();
+        }}>
+        listDiscoveryDevices
+      </Button>
+    </View>
+  );
 };
